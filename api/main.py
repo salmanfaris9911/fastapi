847b08a1eb4d86b2cd018704d5020beb1fd944b3
Salman Faris <salmanfaris@Salmans-MacBook-Air.local>
2024-12-11 09:41:09 +0530

    main file added

--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from prediction import predict_price
 
 app = FastAPI()
 
-@app.get("/")
-def index():
-    return {"message":"Hello, welcome!"}
+@app.get("/api/predict")
+def predict(size,total_sqft,bath,balcony,location):
+    price = list(predict_price(size,total_sqft,bath,balcony,location))[0]
+    return {"message":"Success","Data":{"predicted_price":price},"ErrorCode":0}
